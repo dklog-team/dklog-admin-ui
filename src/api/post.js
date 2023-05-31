0d4c2baf1127c8ac02ac428c02f809015c69552c
hanseunghyeon1 <tmdgus9580@korea.ac.kr>
2023-05-31 17:51:41 +0900

    feat: 페이지 삭제 기능 구현 - 전체 리스트 보여주는 페이지 구현 - 삭제 기능 구현
    #3

--- src/api/post.js
+++ src/api/post.js
@@ -9,6 +9,21 @@ export const getList = () => {
     return request.get(uri, config)
 }
 
+export const getSortList = (param, keywordParam) => {
+    let uri
+    if(keywordParam === null){
+        uri = `/posts?column=${param.reqColumn}&sortDirection=${param.reqDir}`
+    }else{
+        uri = `/posts?column=${param.reqColumn}&keyword=${keywordParam.reqKeyword}&keywordType=${keywordParam.reqKeywordType}&sortDirection=${param.reqDir}`
+    }
+    const config = {
+        headers: {
+            'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiaWF0IjoxNjg1NDI0ODY2fQ.QhXaLPrRDX4dbMVSyuFKIcX3nK4o1DVrdHC6lCCxC4Q',
+        },
+    }
+    return request.get(uri, config)
+}
+
 export const deleteList = (requestData) => {
     const uri = '/posts/resources'
     const config = {
--- src/views/PostAdmin.vue
+++ src/views/PostAdmin.vue
@@ -1,6 +1,28 @@
 <template>
     <div class="flex">
-
+        <label for="selectColumn">컬럼:</label>
+        <select class="select w-full max-w-xs" id="selectColumn" v-model="column" @change="handleDirection">
+            <option value="postId">기본</option>
+            <option value="title">제목</option>
+            <option value="views">조회수</option>
+        </select>
+        <label for="selectDir">방식:</label>
+        <select class="select w-full max-w-xs" id="selectDir" v-model="sortDirection" @change="handleDirection">
+            <option value="">내림차순</option>
+            <option value="asc">오름차순</option>
+        </select>
+        <div class="form-control">
+            <div class="input-group">
+                <select class="select" v-model="keywordType">
+                    <option value="title">제목</option>
+                    <option value="contentText">내용</option>
+                </select>
+                <input type="text" placeholder="Search…" class="input input-bordered w-96" v-model="keyword"/>
+                <button class="btn btn-square" @click="handleDirection">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                </button>
+            </div>
+        </div>
         <button v-if="checkList != ''" class="btn btn-active btn-ghost" @click="deleteListFunc">삭제</button>
     </div>
     <div class="overflow-x-auto w-full">
@@ -36,7 +58,7 @@
                     </div>
                 </td>
                 <td>
-                    {{i.title}}
+                    <a href="#detail" :href="keyword">{{i.title}}</a>
                     <div class="flex">
                         <div class="text-sm opacity-50 overflow-hidden w-96">{{i.previewContent}}</div>
                         <span v-if="i.previewContent" class="text-sm opacity-50">...</span>
@@ -50,12 +72,23 @@
             </tbody>
         </table>
     </div>
+<!--    <a href="#my-modal-2" class="btn">open modal</a>-->
+    <!-- Put this part before </body> tag -->
+    <div class="modal" id="detail">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">{{column}}</h3>
+            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+            <div class="modal-action">
+                <a href="#" class="btn">Yay!</a>
+            </div>
+        </div>
+    </div>
 </template>
 
 
 <script setup>
 import {onBeforeMount, ref} from "vue";
-import {getList, deleteList} from "../api/post.js";
+import {getList, deleteList, getSortList} from "../api/post.js";
 let response
 onBeforeMount(async () => {
     response = await getList()
@@ -67,7 +100,10 @@ const posts = ref([]);
 const checkList = ref([]);
 const deleteOne = [];
 const checkBoolean = ref();
-
+const column = ref("postId");
+const sortDirection = ref("");
+const keywordType = ref("title");
+const keyword = ref("");
 const deleteListFunc = async () => {
     const postIds = checkList.value
     const requestData = {
@@ -100,6 +136,23 @@ const handleAllCheckBox = () =>{
     }else{
         checkList.value = []
     }
+}
+
+const handleDirection = async () => {
+    let reqColumn = column.value
+    let reqDir = sortDirection.value
+    let reqKeywordType = keywordType.value
+    let reqKeyword = keyword.value
+    const param = {
+        reqColumn,
+        reqDir
+    }
+    const keywordParam = {
+        reqKeyword,
+        reqKeywordType
+    };
+    response = await getSortList(param, keywordParam)
+    posts.value = response.data.postList
 }
 </script>
 
