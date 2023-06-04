c180596506d92f0933ec732d4f17c7f6810bbe35
9noeyni9 <koo949494@naver.com>
2023-06-04 15:04:13 +0900

    feat: 댓글 관리 페이지 구현
    - 댓글 정렬, 검색, 삭제 구현
    
    #14

--- src/api/comment.js
+++ src/api/comment.js
@@ -1,12 +1,24 @@
 import request from "./core/request.js";
 
-export const getCommentList = () => {
-    const url = `/comments`
+export const getCommentList = (data) => {
+    const uri = `/comments`
     const config = {
         headers: {
             'Content-Type': 'application/json',
-            'Authorization' : 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiaWF0IjoxNjg1NDI1NTgyfQ.6SOrwY9n0QzBNkl0Tew0RxvnUjmdBNx_YqqFMVntk4s'
+            'Authorization' : 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiaWF0IjoxNjg1NjI2NDYwfQ.aihMsm2qHnmLOS0IKh9JE8sYmpu9ZZ39vyD1X8EFIWY'
+        },
+        params: data,
+    };
+    return request.get(uri,config)
+};
+
+export const deleteCommentList = (requestData) => {
+    const uri = `/comments/resources`
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization' : 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiaWF0IjoxNjg1NjI2NDYwfQ.aihMsm2qHnmLOS0IKh9JE8sYmpu9ZZ39vyD1X8EFIWY'
         },
     };
-    return request.get(url,config)
+    return request.post(uri,requestData,config)
 };
--- src/views/CommentAdmin.vue
+++ src/views/CommentAdmin.vue
@@ -1,79 +1,184 @@
 <template>
-    <div>
-      <div>
-        <div class="overflow-x-auto w-full">
-          <table class="table w-full">
-            <thead>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" class="checkbox" />
-                </label>
-              </th>
-              <th>이름</th>
-              <th>댓글</th>
-              <th>날짜</th>
-              <th></th>
-            </tr>
-            </thead>
+  <div>
+    <h2 class="text-2xl font-bold tracking-tight text-gray-900">댓글 관리 페이지</h2>
+    <p class="mt-2 text-sm leading-8 text-gray-600">dklog 댓글 관리</p>
+  </div>
+  <div>
+    <label for="selectedSortDirection">정렬: </label>
+    <select class="select w-20 max-w-xs" v-model="selectedSortDirection" @change="handleSortChange">
+      <option :value="{'sort':'desc','column':'createdDate'}">최신순</option>
+      <option :value="{'sort':'asc','column':'createdDate'}">오래된 순</option>
+    </select>
 
-            <tbody>
-            <tr v-for="comment in comments" :key="comment.commentId">
-              <th>
-                <label>
-                  <input type="checkbox" class="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div class="flex items-center space-x-3">
-                  <div class="avatar">
-                  </div>
-                  <div>
-                    <div class="font-bold">{{comment.studentName}}</div>
-                    <div class="text-sm opacity-50"></div>
-                  </div>
+    <label for="selectedSemester"> 기수 : </label>
+    <select class="select w-20 max-w-xs" v-model="selectedSemester" @change="handleSemester">
+      <option v-for="number in 3" :value="number">{{ number }}기</option>
+    </select>
+
+    <select class="select w-20 max-w-xs" v-model="selectedColumn">
+      <option value="name">댓글 작성자</option>
+      <option value="content">댓글 내용</option>
+    </select>
+    <input type="text" placeholder="검색" v-model="searchText" class="input w-full max-w-xs"/>
+    <button class="btn btn-circle btn-ghost" @click="handleSearch">검색</button>
+  </div>
+
+  <div>
+    <div class="overflow-x-auto w-full">
+      <table class="table w-full">
+        <thead>
+        <tr>
+          <th>
+            <label>
+              <input type="checkbox" class="checkbox" @click="selectAllCheck"/>
+            </label>
+          </th>
+          <th>이름</th>
+          <th>댓글</th>
+          <th>날짜</th>
+          <th>글 번호</th>
+          <th>
+            <button v-if="commentCheckList.length >0" @click="deleteList">삭제</button>
+          </th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr v-for="comment in comments" :key="comment.commentId">
+          <th>
+            <label>
+              <input v-model="commentCheckList" :value="comment.commentId" type="checkbox" class="checkbox"/>
+            </label>
+          </th>
+          <td>
+            <div class="flex items-center space-x-3">
+              <div class="avatar">
+                <div class="mask mask-squircle w-12 h-12">
+                  <img :src="comment.picture"/>
                 </div>
-              </td>
-              <td>
-                {{comment.content}}
-                <br/>
-              </td>
-              <td>{{comment.createdDate}}</td>
-              <th>
-                <button class="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            </tbody>
+              </div>
+              <div>
+                <div class="font-bold">{{ comment.studentSemester }}기_{{ comment.studentName }}</div>
+                <div class="text-sm opacity-50"></div>
+              </div>
+            </div>
+          </td>
+          <td>
+            {{ comment.content }}
+            <br/>
+          </td>
+          <td>작성일 : {{ comment.createdDate }}<br>
+            <div v-if="comment.modifiedDate" class="text-gray-500">수정일 : {{ comment.modifiedDate }}</div>
+          </td>
+          <td>{{ comment.postId }}</td>
+          <th>
+            <label class="btn btn-ghost" @click="deleteOne(comment.commentId)">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                   stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"/>
+              </svg>
+            </label>
+          </th>
+        </tr>
+        </tbody>
 
-            <tfoot>
-            <tr>
-              <th></th>
-              <th>이름</th>
-              <th>댓글</th>
-              <th>날짜</th>
-              <th></th>
-            </tr>
-            </tfoot>
+        <tfoot>
+        <tr v-if="comments.length === 0">존재하는 댓글이 없습니다.</tr>
+        <tr>
+          <th></th>
+          <th>이름</th>
+          <th>댓글</th>
+          <th>날짜</th>
+          <th>글 번호</th>
+          <th></th>
+        </tr>
+        </tfoot>
 
-          </table>
-        </div>
-        </div>
-      </div>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import {onBeforeMount, ref} from "vue";
-import {getCommentList} from "../api/comment.js"
+import {deleteCommentList, getCommentList} from "../api/comment.js"
 
-const comments = ref("");
+const comments = ref('');
+const selectedColumn = ref('');
+const selectedSemester = ref('');
+const selectedSortDirection = ref('');
+const commentCheckList = ref([]);
+const searchText = ref('');
+
+const data = {
+  column: 'createdDate',
+  sortDirection: '',
+  name: '',
+  semester: '',
+  content: '',
+};
+let response;
 
 onBeforeMount(async () => {
-  const response = await getCommentList();
-  console.log(response.data)
-  comments.value = response.data.commentList
-  console.log(response.data.commentList.picture)
-
+  response = await getCommentList({});
+  comments.value = response.data.commentList;
 });
+
+const selectAllCheck = async () => {
+  const selectAllCheckbox = commentCheckList.value.length === comments.value.length;
+
+  if (selectAllCheckbox) {
+    commentCheckList.value = [];
+  } else {
+    commentCheckList.value = comments.value.map(comment => comment.commentId);
+  }
+};
+
+const deleteList = async () => {
+  const commentIds = commentCheckList.value;
+  const requestData = {commentIds}
+
+  console.log(requestData.value);
+  await deleteCommentList(requestData);
+  commentCheckList.value = [];
+
+  response = await getCommentList(data);
+  comments.value = response.data.commentList;
+};
+
+const deleteOne = async (commentId) => {
+  const requestData = {commentIds: [commentId]}
+
+  await deleteCommentList(requestData);
+
+  response = await getCommentList(data);
+  comments.value = response.data.commentList;
+};
+
+const handleSortChange = async () => {
+  data.sortDirection = selectedSortDirection.value.sort;
+  data.column = selectedSortDirection.value.column;
+
+  response = await getCommentList(data);
+  comments.value = response.data.commentList;
+};
+
+const handleSemester = async () => {
+  data.semester = selectedSemester.value;
+  response = await getCommentList(data);
+  comments.value = response.data.commentList;
+};
+
+const handleSearch = async () => {
+  if (selectedColumn.value === 'name')
+    data.name = searchText.value;
+  else
+    data.content = searchText.value;
+
+  response = await getCommentList(data);
+  comments.value = response.data.commentList;
+};
 </script>
 
 <style scoped>
