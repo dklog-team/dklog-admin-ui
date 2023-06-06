3b3ace87ba5662741f9040eac6934b6c192c2f3e
hanseunghyeon1 <tmdgus9580@korea.ac.kr>
2023-06-06 11:11:16 +0900

    feat: 게시글 관리 페이지 UI 수정
    - UI 수정
    
    #39

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import './style.css'
 import App from './App.vue'
 import router from "./routes";
 import {createPinia} from "pinia";
+import VueHighlightJS from 'vue3-highlightjs'
+import 'highlight.js/styles/androidstudio.css'
 
 const pinia = createPinia()
 
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App).use(router).use(pinia).use(VueHighlightJS).mount('#app')
